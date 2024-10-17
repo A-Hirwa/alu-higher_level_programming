252ce9c55e3dfffcde9334c781335ba00dfeffc1
A-Hirwa <a.hirwa1@alustudent.com>
2024-10-17 12:28:06 -0700

    print alphabet

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alphabet in ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]:
-    print("{}".format(alphabet), end="")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
