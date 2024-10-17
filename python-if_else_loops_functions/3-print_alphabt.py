b3febdfbf33ffa13096a06378f36d75e6fcc3bda
A-Hirwa <a.hirwa1@alustudent.com>
2024-10-17 12:44:33 -0700

    print without q and e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(97 , 123):
-    if chr(i) in ("e" , "q"):
+for i in range(97, 123):
+    if chr(i) in ("e", "q"):
         continue
     else:
         print("{}".format(chr(i)), end="")
