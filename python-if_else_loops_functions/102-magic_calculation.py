dd9a43ccbe94f3f478eeb8740bb9ac5f832890f4
A-Hirwa <a.hirwa1@alustudent.com>
2024-10-18 05:24:27 -0700

    magic calculations

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
+    print(f"a = {a}, b = {b}, c = {c}")
     if a < b:
+        print("Returning c")
         return c
     elif b > c:
+        print("Returning a + b")
         return a + b
     else:
+        print("Returning (a * b) - c")
         return (a * b) - c
