d0a19667470b211c48e02b4123e64414dd73df1c
A-Hirwa <a.hirwa1@alustudent.com>
2024-10-21 10:15:35 -0700

    count arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     import sys
-    if (len(sys.arg)-1) == 0:
+    if (len(sys.argv)-1) == 0:
         print("0 arguments.")
     elif (len(sys.argv)-1) == 1:
         print("1 argument:")
