9fdcc06a76213bb18e207a1dfc15ed8c613a1122
Lightstats <hirwa12ariane@gmail.com>
2024-10-22 17:47:09 +0200

    replace part of string

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,10 +2,10 @@
 
 
 def no_c(my_string):
-  new_string = []
-  for char in my_string:
-    if char in ("c", "C"):
-      continue
-    else:
-      new_string.append(char)
-  return "".join(new_string)
+   new_string = []
+   for char in my_string:
+     if char in ("c", "C"):
+        continue
+     else:
+        new_string.append(char)
+     return "".join(new_string)
