7fe8343fc799d16ae5f820ea1a1d684b118c8101
A-Hirwa <a.hirwa1@alustudent.com>
2024-10-22 20:02:35 +0200

    Update 11-delete_at.py

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,8 +2,8 @@
 
 
 def delete_at(my_list=[], idx=0):
-    if idx >= 0 and idx < len(my_list):
-        del my_list[idx]
+	if idx >= 0 and idx < len(my_list):
+    	del my_list[idx]
         return my_list
     else:
       return my_list
