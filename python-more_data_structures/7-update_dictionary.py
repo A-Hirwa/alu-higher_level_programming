1c05a1f32385ef14c4cd30ebcc9bd0055617dc0e
A-Hirwa <a.hirwa1@alustudent.com>
2024-10-24 07:21:23 -0700

    delte found key

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    new_dict = {}
+    for key, value in a_dictionary.items():
+        if key == key:
+            new_dict[key] = value
+        else:
+            new_dict[key] = value
+    return new_dict
