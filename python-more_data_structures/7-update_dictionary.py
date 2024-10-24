0c04bc8a7d46e1d38482a662f1da4bd2f213727a
A-Hirwa <a.hirwa1@alustudent.com>
2024-10-24 08:22:24 -0700

    update

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dict = {}
-    for key, value in a_dictionary.items():
-        if key == key:
-            new_dict[key] = value
-        else:
-            new_dict[key] = value
-    return new_dict
+    a_dictionary[key] = value
+    return a_dictionary
