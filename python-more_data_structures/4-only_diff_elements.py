bc2294d0f40f11fbb9999fa8955cd3ea3afc5dfa
A-Hirwa <a.hirwa1@alustudent.com>
2024-10-23 22:00:19 -0700

    add different elements

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     new_set = set()
-    for element in set_1:
-        if element in set_2:
-            continue
-        else:
-            new_set.add(element)
+    for s in (set_1, set_2):
+        for element in s:
+            if element in set_2 and element in set_1:
+                continue
+            else:
+                new_set.add(element)
     return new_set
