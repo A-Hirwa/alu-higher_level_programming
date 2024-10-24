51be62fb8f7fc951d87b5bc80290f640ebd595b1
A-Hirwa <a.hirwa1@alustudent.com>
2024-10-23 21:46:52 -0700

    return a combined set

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     new_set = set()
-    for element in (set_1, set_2):
-        new_set.add(element)
+    for s in (set_1, set_2):
+        for element in s:
+            new_set.add(element)
     return new_set
