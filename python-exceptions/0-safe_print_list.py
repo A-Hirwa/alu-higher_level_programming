45b1635a741abe11d7057bd926ffe4d7291d837a
A-Hirwa <a.hirwa1@alustudent.com>
2024-10-29 09:31:42 -0700

    Print integer only

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range (x):
-        try:
-            print("{:d}".format(my_list[i]),end="")
-        except IndexError:
-            print("{}".format(my_list))
+  num = []
+  for i in range (x):
+    try:
+      num.append(my_list[i])
+    except:
+      break
+  return num 
