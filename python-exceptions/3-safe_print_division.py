33ce8cef306f2e3c6e989749df5ee5f01ab25597
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-05 06:49:16 -0800

    print results

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -8,4 +8,4 @@ def safe_print_division(a, b):
             result = None
             print("Inside result: {}".format(result))
     finally:
-        print("{:d} / {:d} = {}".format(a, b, result))
+        return result
