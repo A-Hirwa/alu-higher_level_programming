028e80f3241f8521776a616fb70b22e2a35ed6cd
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-05 07:21:52 -0800

    return results

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,7 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
             try:
                 results.append(my_list_1[i] / my_list_2[i])
             except TypeError:
-                print("Wrong type")
+                print("wrong type")
                 results.append(0)
             except ZeroDivisionError:
                 print("division by 0")
