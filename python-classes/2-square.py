5da6c904af19f113fcf9ff48781e7dd6f39a203d
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-06 04:11:58 -0800

    add size greater than 0

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Square class to represent a class"""
+
+
+class Square:
+    """Define square class with size greater than 0"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
