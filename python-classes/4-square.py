e1384e0f485c8ee317777b5eae76fb110304289b
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-06 04:25:11 -0800

    return size

---
+++ python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Square class to represent a class"""
+
+
+class Square:
+    """Define square class with size greater than 0"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+
+    def area(self):
+        """Return area of square"""
+        return self.__size ** 2
+
+
+    @property
+    def size(self):
+        """Return size"""
+        return self.__size
