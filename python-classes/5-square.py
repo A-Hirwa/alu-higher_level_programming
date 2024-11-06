d12f54ef3792e79398ffd9fa8aced49f405daada
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-06 04:35:12 -0800

    print square

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Square class to represent a class"""
+
+
+class Square:
+    """Define square class with size greater than 0"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+  
+    @property
+    def size(self):
+        """Return size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size"""
+        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+          
+    def area(self):
+        """Return area of square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print square"""
+        for i in range(self.__size):
+            print("#" * self.__size)
