25b47b11f10e4fa878a0abdd8b9c970b11cdc15e
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-07 06:19:16 -0800

    inherit from list

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Creates class that inherits from list builtin functions"""
+
+
+class MyList(list):
+    """Class that inherits from list"""
+
+
+    def print_sorted(self):
+        """Prints the list in ascending order"""
+        print(sorted(self))
