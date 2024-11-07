6c0bdf51ad6eb4774c3df212f5b1c18293fdb090
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-07 07:08:22 -0800

    check subclass

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Creates a function that tests if objects are subclasses of a class'''
+
+
+def inherits_from(obj, a_class):
+    '''Function returns true if obj is an sub class and false if not    
+    Args:
+        obj: object to check
+        a_class: class to check
+    '''
+    x = issubclass(obj, a_class)
+    return x
