77e6b0d269fca2c8172e7a30d2a766c51658c4f0
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-07 07:22:21 -0800

    area exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Creating adding area function to class'''
+
+
+class BaseGeometry:
+    '''Base class'''
+    
+    def area(self):
+        '''raises an area exception'''
+        raise Exception("area() is not implemented")
