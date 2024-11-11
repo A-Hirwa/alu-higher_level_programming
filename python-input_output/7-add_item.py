43005e0a54b17ddee90447cda3aae67d46af1655
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-11 07:35:10 -0800

    add and save list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -13,5 +13,11 @@ save_to = __import__('5-save_to_json_file.py').save_to_json_file
 load_fro = __import__('6-load_from_json_file.py').load_from_json_file
 
 
+"""
+This function loads the JSON file
+"""
 new_file = load_fro(add_item.json)
+"""
+This function saves the JSON file
+"""
 save_to(new_file)
