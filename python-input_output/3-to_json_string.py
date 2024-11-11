870f587a4dc363ecabe89a8f8b37df84d76fd9da
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-11 06:37:45 -0800

    convert to JSON

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+3-to_json_string.py
+
+This module has a function that converts a string to JSON
+"""
+
+
+def to_json_string(my_obj):
+    """
+    This functions converts a string to JSON
+
+    Args:
+        my_obj: the string to convert
+    """
+    result = json.loads(my_obj)
+    return result
