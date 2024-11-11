2a826ac3d3ca1c7ef1eedbb6abcf1925e25b7dfd
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-10 21:27:56 -0800

    add string to file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+1-write_file.py
+
+This module as a function that adds a string to a file
+"""
+def append_write(filename="", text=""):
+    """
+    This function adds a string to a file
+
+    Args:
+        filename: the file added to
+        text: the string added to the file
+    """
+    with open(filename, mode="w", encoding="UTF-8") as file:
+        file.write(text)
+        return len(text)
