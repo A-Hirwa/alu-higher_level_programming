95a5e6ac12156049d239e69da78177639e2db0d0
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-11 08:22:42 -0800

    return dict for instance

---
+++ python-input_output/9-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+9-student.py
+
+This module has a class with a function that returns a dictionary representation of the class
+"""
+
+
+class student:
+    """
+    This is a class that defines a student
+
+    attributes:
+                first_name
+                last_name
+                age
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initializing class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    def to_json(self):
+        """
+        function returns dictionary representation of a class
+
+        Args:
+            self
+        """
+        return self.__dict__
