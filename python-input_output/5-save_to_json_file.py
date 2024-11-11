167017819e12658e9630f404260d1c88b143507c
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-11 06:59:45 -0800

    add to JSON file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+5-save_to_json_file.py
+
+This module has writes an object to a JSON text file
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    This function writes an object to a JSON file
+
+    Args:
+        my_obj: the object to add to the file
+        filename: the name of the file
+    """
+    with open(filename,"w") as f:
+        json.dumps(my_obj, f)
