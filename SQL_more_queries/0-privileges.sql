53ac6b18fe9515a51b9c910e78cc1aaa9276c98e
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-15 05:30:25 -0800

    check privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
 -- Lists all privileges of the users user_0d_1 and user_0d_2.
+SELECT 'Privileges for user_0d_1@localhost' AS User_Info;
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SELECT 'Privileges for user_0d_2@localhost' AS User_Info;
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
