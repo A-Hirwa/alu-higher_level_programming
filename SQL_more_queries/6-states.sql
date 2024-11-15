8fa637d0e259b19f9e700c95abc0d14a07ebd17b
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-15 07:47:29 -0800

    additional

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- create table and database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS states(
+	id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256));
