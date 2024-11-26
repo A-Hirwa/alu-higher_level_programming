e834f1e9d042b00c80d1091d036712ae92af2a11
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-26 10:59:04 +0000

    display 200 body

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 # display the body of a file 
-[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ] && curl -s "$1"
+[ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" -eq 200 ] && curl -s -L "$1"
