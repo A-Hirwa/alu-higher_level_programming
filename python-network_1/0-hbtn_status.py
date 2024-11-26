f71987df4bc0aeee0d482d34a30392259ebf5b0f
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-26 13:51:27 +0000

    another

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
-'''a Python script that fetches a url'''
-
+"""Fetches https://alu-intranet.hbtn.io/status using urllib"""
 
 import urllib.request
 
+url = "https://alu-intranet.hbtn.io/status"
 
-url = 'http://0.0.0.0:5050/status'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-    '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
-    '\n    Chrome/99.0.4844.84 Safari/537.36',
-}
-
-req = urllib.request.Request(url, headers=headers)
-
-if __name__ == '__main__':
-    with urllib.request.urlopen(req) as response:
+if __name__ == "__main__":
+    with urllib.request.urlopen(url) as response:
         content = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
