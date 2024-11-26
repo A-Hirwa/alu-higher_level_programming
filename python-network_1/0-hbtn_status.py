59e3684e75dcf5ac90bad213a1118bdaec4ace38
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-26 13:32:10 +0000

    update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@
 import urllib.request
 
 
-url = 'https://alu-intranet.hbtn.io/status'
+url = 'http://0.0.0.0:5050/status'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
