4bf5312db19c0713ca765c0f1db3f8c53af72e4f
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-26 13:57:49 +0000

    new

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status using requests"""
+"""Fetches status from two URLs using requests"""
 
 import requests
 
 if __name__ == "__main__":
-    response = requests.get("https://alu-intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    urls = [
+        "https://alu-intranet.hbtn.io/status",
+        "http://0.0.0.0:5050/status",
+    ]
+
+    if url in urls:
+        response = requests.get(url)
+        print("Fetching from: {}".format(url))
+        print("Body response:")
+        print("\t- type: {}".format(type(response.text)))
+        print("\t- content: {}".format(response.text))
+        print()
--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -8,11 +8,12 @@ if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     q = sys.argv[1] if len(sys.argv) > 1 else ""
     response = requests.post(url, data={"q": q})
+
     try:
         json_response = response.json()
         if json_response:
-            print("[{}] {}".format(json_response.get("id"),
-	      json_response.get("name")))
+            print("[{}] {}".format(
+                json_response.get("id"), json_response.get("name")))
         else:
             print("No result")
     except ValueError:
