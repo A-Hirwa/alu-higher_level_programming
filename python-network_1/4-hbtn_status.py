dbaa4ea1302e95adb3746206e4e2f4f6743b3ba9
A-Hirwa <a.hirwa1@alustudent.com>
2024-11-27 11:07:40 +0000

    update

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-"""Fetches status from a dynamically selected URL using requests"""
-
+"""Fetches url
+using the request moduel
+"""
 import requests
 
-if __name__ == "__main__":
-    # Define the two URLs to try
-    urls = [
-        "https://alu-intranet.hbtn.io/status",
-        "http://0.0.0.0:5050/status",
-    ]
 
-    for url in urls:
-        try:
-            # Attempt to fetch the URL
-            response = requests.get(url, timeout=5)
-            # If successful, print the response and break the loop
-            print("Body response:")
-            print("\t- type: {}".format(type(response.text)))
-            print("\t- content: {}".format(response.text))
-            break
-        except requests.exceptions.RequestException:
-            # If the URL fails, try the next one
-            continue
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
+    res = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
